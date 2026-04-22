62f0768ad6705ffe49aff740c6f3f2b75db1d4ad
sorawittj-hue <sorawittj@gmail.com>
2026-04-22 23:44:06 +0700

    Updates

---
+++ assets/chevron-right-Dp6ojsqR.js
@@ -0,0 +1,7 @@
+import{c as t}from"./index-CDjUx6Zi.js";/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
+//# sourceMappingURL=chevron-right-Dp6ojsqR.js.map
