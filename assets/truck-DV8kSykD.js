5fc509e4d88cbcbbdab9e049063330dd39907b44
sorawittj-hue <sorawittj@gmail.com>
2026-07-25 18:07:40 +0700

    Updates

--- assets/truck-DV8kSykD.js
+++
@@ -1,2 +0,0 @@
-import{b as c}from"./index-ZH-ui32Y.js";const r=c("Truck",[["path",{d:"M5 18H3c-.6 0-1-.4-1-1V7c0-.6.4-1 1-1h10c.6 0 1 .4 1 1v11",key:"hs4xqm"}],["path",{d:"M14 9h4l4 4v4c0 .6-.4 1-1 1h-2",key:"11fp61"}],["circle",{cx:"7",cy:"18",r:"2",key:"19iecd"}],["path",{d:"M15 18H9",key:"1lyqi6"}],["circle",{cx:"17",cy:"18",r:"2",key:"332jqn"}]]);export{r as T};
-//# sourceMappingURL=truck-DV8kSykD.js.map
